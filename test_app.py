580e760139ff3aff7567ff827a382976a162e82a
Charlie Dineen <124352606@umail.ucc.ie>
2026-03-22 17:35:58 +0000

    feat: fix2

---
+++ test_app.py
@@ -0,0 +1,16 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+def test_health(client):
+    response = client.get("/health")
+    assert response.status_code == 200
+
+def test_status(client):
+    response = client.get("/status")
+    assert response.status_code == 200
